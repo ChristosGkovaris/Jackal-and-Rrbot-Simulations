dc8f21f1a63fae3d298122ab33663417dbd3d1b6
Christos Gkovaris <christosgkovariscs52@gmail.com>
2024-10-25 11:45:30 +0300

    Update move_jackal.py

--- Code/move_jackal.py
+++ Code/move_jackal.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# Team 01
-# Spanou Maria, AM: 5351
-# Gkovaris Christos-Grigorios, AM: 5203
-
 import ast                                       # Module for safely evaluating string expressions to Python objects
 import rospy                                     # ROS Python client library
 import argparse                                  # Module for parsing command-line arguments
